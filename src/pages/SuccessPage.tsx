49e0ecbe316f5e8324c85bf431a57f793a3d6b98
badzo7 <canva2023compte@gmail.com>
2025-08-19 15:53:18 +0400

    Update SuccessPage.tsx translation to english

--- src/pages/SuccessPage.tsx
+++ src/pages/SuccessPage.tsx
@@ -7,15 +7,15 @@ const SuccessPage: React.FC = () => {
     return (
         <div className="pt-24 pb-16 container-custom max-w-xl mx-auto text-white text-center">
             <div className="bg-neutral-900 p-8 rounded-xl shadow-lg">
-                <h1 className="text-3xl font-bold mb-4">Merci pour votre commande !</h1>
+                <h1 className="text-3xl font-bold mb-4">Thank you for your order Champion !</h1>
                 <p className="text-neutral-400 mb-6">
-                    Votre commande a été confirmée. Un e-mail de confirmation vous sera envoyé sous peu.
+                    Your Order Has Been Confirmed. A Confirmation Email Will Be Sent To You Shortly.
                 </p>
                 <button
                     onClick={() => navigate('/')}
                     className="bg-yellow-500 hover:bg-yellow-400 text-black font-semibold py-3 px-6 rounded-lg text-lg transition"
                 >
-                    Retour à l’accueil
+                    Return to main page
                 </button>
             </div>
         </div>
